260984dd09daa5b569e7b368df7c65e7209f7766
jonathanRinciari <jrinciari@greencheckverified.com>
2019-09-09 23:12:31 -0400

    3 files modified

--- src/app/title/title.component.ts
+++ src/app/title/title.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output, Input} from '@angular/core';
 
 @Component({
   selector: 'app-title',
@@ -6,6 +6,7 @@ import { Component, OnInit, EventEmitter, Output } from '@angular/core';
   styleUrls: ['./title.component.css']
 })
 export class TitleComponent implements OnInit {
+  @Input() _title: string;
   @Output() onClick = new EventEmitter();
 
   title = '';
