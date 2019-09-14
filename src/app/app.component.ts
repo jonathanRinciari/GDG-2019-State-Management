88478b5bbc98c2975f268d078f7b2d74cd46725d
jonathanRinciari <jrinciari@greencheckverified.com>
2019-09-13 22:05:20 -0400

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import {StateService} from '../services/state.service'
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent implements onInit {
-  appTitle: sting;
+  appTitle: string;
   constructor(
     private stateService: StateService
   ) {}
